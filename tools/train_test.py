f524f610db69ba10e2dbb0ffc0542e38378a133f
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2018-05-18 16:16:39 -0300

    refactor plot learning curve function

--- tools/train_test.py
+++ tools/train_test.py
@@ -1,4 +1,3 @@
-
 import logging
 import config
 import models
@@ -7,61 +6,10 @@ import numpy as np
 import pandas as pd
 import os
 import time
-import matplotlib.pyplot as plt
 import multiprocessing
 from ctypes import c_float # for retrieving test results
 from itertools import product # for grid_search function
-
-def plot_learning_curve(learning_curve, model_info, export_path=None):
-    """Plots the learning curve following `model_info`. Allows the option to export an SVG figure
-    to some path.
-    """
-    data = learning_curve
-    if model_info['log_type'] == 'epoch':
-        n_epochs = data.epoch.max()
-        y_min = data.epoch_loss.min()
-        y_max = data.epoch_loss.iloc[model_info['n_epochs'] // 10]
-
-        fig, ax1 = plt.subplots()
-
-        ax1.set(title='Training Loss and Validation Accuracy',
-                xlabel='Epoch',
-                ylabel='Loss',
-                xlim=[0, model_info['n_epochs']],
-                ylim=[y_min, y_max],
-               )
-        ax1.plot(data.epoch, data.epoch_loss, label='Loss')
-        ax1.legend(loc=2)
-
-        ax2 = ax1.twinx()
-        ax2.plot(data.epoch, data.valid_acc, label='Valid Acc', color='orange')
-        ax2.set(ylabel='Accuracy')
-        ax2.legend(loc=1)
-
-        fig.set_size_inches(18, 12)
-        if export_path != None: fig.savefig("{}/learning_curve.svg".format(export_path))
-
-    elif model_info['log_type'] == 'batch':
-        fig, ax1 = plt.subplots()
-
-        ax1.set(title='Training Loss and Validation Accuracy',
-                xlabel='Batch (absolute)',
-                ylabel='Loss',
-               )
-        ax1.plot(data.batch_loss, label='Batch Loss')
-        ax1.legend(loc=2)
-
-        ax2 = ax1.twinx()
-        ax2.plot(data.valid_acc, label='Valid Acc', color='orange')
-        ax2.set(ylabel='Accuracy')
-        ax2.legend(loc=1)
-
-        fig.set_size_inches(18, 12)
-        if export_path != None: fig.savefig("{}/learning_curve.svg".format(export_path))
-    else:
-        return ValueError('log_type should be defined in model_info.')
-    return fig
-
+from plots import plot_learning_curve
 
 def setup_config(model_info):
     """Create config instance and set variables/hyperparameters.
