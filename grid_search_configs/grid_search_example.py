afce3489aaa76661ee21ab90a11d9d44ae3b928c
arthurcgusmao <arthurcgusmao@hotmail.com>
2018-05-22 19:32:01 -0300

    fix import in grid_search.py files

--- grid_search_configs/grid_search_example.py
+++ grid_search_configs/grid_search_example.py
@@ -1,4 +1,10 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
 from tools import train_test
+
 
 train_test.grid_search({
 
