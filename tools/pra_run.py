494f5f4ff5a5f92537feddf2f6f7772112229dcc
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2018-06-08 01:21:30 -0300

    update pra_run to new pra config

--- tools/pra_run.py
+++ tools/pra_run.py
@@ -55,7 +55,7 @@ def read_generate_train_data(corrupted_filepath):
 ##### Functions #####
 
 def extract_features(emb_import_path, neg_rate, bern, feature_extractors, cuda_device=0, use_ids=False,
-        g_hat_info=None, data_to_use='both', dataset_path=None):
+        g_hat_info=None, data_to_use='onefold', dataset_path=None):
     """Extract features using PRA library.
 
     Arguments:
@@ -70,7 +70,7 @@ def extract_features(emb_import_path, neg_rate, bern, feature_extractors, cuda_d
     instead of names
     - `g_hat_info` (dict): see below
     - `data_to_use` (string): data that PRA should consider when extracting features. Should be one
-    of `both`, `training`, or `testing`.
+    of `onefold` or `allfolds`.
 
     If extracting features from a graph predicted by the embedding model, a dict should be passed
     in `g_hat_info` with the following key-value pairs:
@@ -289,107 +289,12 @@ def extract_features(emb_import_path, neg_rate, bern, feature_extractors, cuda_d
 
 
     ## Extract Features
-    bash_command = '/home/arthurcgusmao/Projects/xkbc/algorithms/OpenKE/tools/run_pra_n_times.sh {} {} {}'\
-        .format(pra_dir_path, spec_name, n_relations)
-    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    print output, error
+    bash_command = '/home/arthurcgusmao/Projects/xkbc/algorithms/OpenKE/tools/run_pra.sh {} {}'.format(pra_dir_path, spec_name)
+    n_runs = n_relations; if data_to_use == 'onefold': n_runs *= 3
+    for r in range(n_runs):
+        print("Running #{}: {}".format(r, bash_command))
+        process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+        print output, error
+
     print("\nIf PRA run successfully, features were extracted and saved into `{}`.".format(pra_dir_path))
-
-
-#
-# def extract_features_from_dataset(dataset_path, neg_rate, bern, feature_extractors, use_ids=False, data_to_use='both'):
-#     """Extracts features from a dataset. This is useful when one wants to run PRA directly on an input
-#     graph.
-#     """
-#     pra_path = os.path.abspath(dataset_path + '/pra')
-#     distribution = 'bern' if bern else 'unif'
-#     corrupted_filename = 'train2id_{}negrate_{}.txt'.format(neg_rate, distribution)
-#     corrupted_dirpath = dataset_path + '/corrupted/'
-#     corrupted_filepath = corrupted_dirpath + corrupted_filename
-#     split_name = 'original_data' if not use_ids else 'original_data2id'
-#     pra_graph_input_dir = dataset_path + '/pra_graph_input/' if not use_ids else dataset_path + '/pra_graph_input2id/'
-#     pra_graph_input_dir = os.path.abspath(pra_graph_input_dir)
-#     g_type = 'g' if not use_ids else 'g2id'
-#
-#     # Handle feature extraction strings and spec name
-#     feature_extractor_dict = {
-#         'pra': 'PraFeatureExtractor',
-#         'onesided': 'OneSidedPathAndEndNodeFeatureExtractor',
-#         'anyrel': 'AnyRelFeatureExtractor'
-#     }
-#     spec_name = split_name + '__'
-#     feat_list = []
-#     for feat in feature_extractors:
-#         spec_name += '_' + feat
-#         feat_list.append('"{}"'.format(feature_extractor_dict[feat]))
-#     feat_extractor_string = ','.join(feat_list)
-#
-#     ensure_dir(pra_path)
-#     ensure_dir(pra_path + '/experiment_specs')
-#
-#     spec = """
-#     {{
-#         "graph": {{
-#             "name": "{}",
-#             "relation sets": [
-#                 {{
-#                     "is kb": false,
-#                     "relation file": "{}/train.tsv"
-#                 }},
-#                 {{
-#                     "is kb": false,
-#                     "relation file": "{}/valid.tsv"
-#                 }}
-#             ]
-#         }},
-#         "split": "{}",
-#         "operation": {{
-#             "type": "create matrices",
-#             "features": {{
-#                 "type": "subgraphs",
-#                 "path finder": {{
-#                     "type": "BfsPathFinder",
-#                     "number of steps": 2
-#                 }},
-#                 "feature extractors": [
-#                     {}
-#                 ],
-#                 "feature size": -1
-#             }},
-#             "data": "{}"
-#         }},
-#         "output": {{ "output matrices": true }},
-#     }}
-#     """.format(g_type, pra_graph_input_dir, pra_graph_input_dir, split_name, feat_extractor_string, data_to_use)
-#     spec_fpath = '{}/experiment_specs/{}.json'.format(pra_path, spec_name)
-#     with open(spec_fpath, 'w') as f:
-#         f.write(spec)
-#     print "Spec file written: {}".format(spec_fpath)
-#
-#     # read data and metadata
-#     train2id = read_generate_train_data(corrupted_filepath)
-#     valid2id, test2id = read_valid_and_test_data(dataset_path)
-#     entity2id, id2entity = dataset_tools.read_name2id_file(dataset_path + '/entity2id.txt')
-#     relation2id, id2relation = dataset_tools.read_name2id_file(dataset_path + '/relation2id.txt')
-#     n_relations = len(relation2id)
-#
-#     # decode from id to names if necessary
-#     if not use_ids:
-#         decode_from_id_to_names([train2id, valid2id, test2id])
-#         # WARNING: at this stage we have transformed the dataframes,
-#         #   and entities and relations are not represented by ids anymore
-#
-#     # create Split
-#     pra_setup.create_split(
-#             {'train': train2id, 'valid': valid2id, 'test': test2id},
-#             splits_dirpath=pra_path+'/splits',
-#             split_name=split_name)
-#
-#     # extract features
-#     bash_command = '/home/arthurcgusmao/Projects/xkbc/algorithms/OpenKE/tools/run_pra_n_times.sh {} {} {}'\
-#         .format(pra_path, spec_name, n_relations)
-#     process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
-#     output, error = process.communicate()
-#     print output, error
-#     print("\nIf PRA run successfully, features were extracted and saved into `{}`.".format(pra_path))
---
+++ tools/run_pra.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+(cd /home/arthurcgusmao/Projects/xkbc/algorithms/pra/; sbt "runMain edu.cmu.ml.rtw.pra.experiments.ExperimentRunner $1 $2")
