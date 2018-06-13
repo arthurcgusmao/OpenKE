390ffbae72a3d7804f775652e72a2dd247ad3c17
arthurcgusmao <arthurcgusmao@hotmail.com>
2018-06-13 11:52:31 -0300

    extract features w/ PRA Pipeline

---
+++ examples/extract_features/pipeline.py
@@ -0,0 +1,50 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+grandparentdir = os.path.dirname(parentdir)
+sys.path.insert(0,grandparentdir)
+
+from tools import pra_run
+
+
+def pipeline(emb_import_paths):
+    for emb_import_path in emb_import_paths:
+        pra_run.extract_features(
+            emb_import_path=emb_import_path,
+            neg_rate=2,
+            bern=True,
+            feature_extractors=['pra'],
+            cuda_device=0,
+            use_ids=False,
+            g_hat_info=None,
+            data_to_use='onefold'
+        )
+        pra_run.extract_features(
+            emb_import_path=emb_import_path,
+            neg_rate=2,
+            bern=True,
+            feature_extractors=['pra'],
+            cuda_device=0,
+            use_ids=False,
+            g_hat_info={'knn_k': 3},
+            data_to_use='onefold'
+        )
+        pra_run.extract_features(
+            emb_import_path=emb_import_path,
+            neg_rate=2,
+            bern=True,
+            feature_extractors=['pra'],
+            cuda_device=0,
+            use_ids=False,
+            g_hat_info={'knn_k': 5},
+            data_to_use='onefold'
+        )
+
+
+emb_import_paths = [
+    '/home/arthurcgusmao/Projects/xkbc/algorithms/OpenKE/results/WN11/TransE/1527008113',
+    '/home/arthurcgusmao/Projects/xkbc/algorithms/OpenKE/results/FB13/TransE/1527033688',
+    '/home/arthurcgusmao/Projects/xkbc/algorithms/OpenKE/results/NELL186/TransE/1526711822',
+]
+pipeline(emb_import_paths)
+
