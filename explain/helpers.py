ca7eb7a3fa9fbbcb99e0bef8ed141a27c5fc9996
arthurcgusmao <arthurcgusmao@hotmail.com>
2018-06-15 21:24:01 -0300

    nanmean without weights where necessary

--- explain/helpers.py
+++ explain/helpers.py
@@ -127,10 +127,10 @@ def get_metrics(dataframe_path, model_info, split):
     # calculate averages ignoring nans (which can happen for metrics that are weighted or filtered).
     for metric in relevant_test_metrics:
         metrics_dict[metric + '_micro'] = nanaverage(per_relation_metrics[metric], weights=per_relation_metrics['test_weights'])
-        metrics_dict[metric + '_macro'] = nanaverage(per_relation_metrics[metric])
+        metrics_dict[metric + '_macro'] = np.nanmean(per_relation_metrics[metric])
     for metric in relevant_train_metrics:
         metrics_dict[metric + '_micro'] = nanaverage(per_relation_metrics[metric], weights=per_relation_metrics['train_weights'])
-        metrics_dict[metric + '_macro'] = nanaverage(per_relation_metrics[metric])
+        metrics_dict[metric + '_macro'] = np.nanmean(per_relation_metrics[metric])
 
     metrics_dict['dataset_name'] = model_info['dataset_name']
     metrics_dict['model_name'] = model_info['model_name']
@@ -139,6 +139,6 @@ def get_metrics(dataframe_path, model_info, split):
 
     return metrics_dict
 
-def nanaverage(a, weights):
+def nanaverage(a, weights=None):
     indices = ~np.isnan(a)
     np.average(a[indices], weights=weights[indices])
